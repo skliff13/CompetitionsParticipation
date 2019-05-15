9bccec1db68f9db1cec981c727a313b7fb48245e
skliff13 <vitali.liauchuk@gmail.com>
2019-05-15 16:27:24 +0300

    + script for table

---
+++ ImageCLEF2019/Scripts/metadata/aux_make_svr_table.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+def svr_table():
+    df1 = pd.read_csv('TrainingSet_metaData.csv')
+    df2 = pd.read_csv('TestSet_metaData.csv')
+
+    fields = ['md_Disability', 'md_Relapse', 'md_SymptomsOfTB', 'md_Comorbidity', 'md_Bacillary',
+              'md_DrugResistance', 'md_HigherEducation', 'md_ExPrisoner', 'md_Alcoholic', 'md_Smoking']
+
+    for field in fields:
+        s = field[3:] + ' & '
+        s += str(sum(df1[field])) + ' & '
+        s += str(sum(df2[field])) + ' \\\\'
+
+        print(s)
+
+
+def ctr_table():
+    df1 = pd.read_csv('TrainingSet_metaData.csv')
+
+    fields = ['CTR_LeftLungAffected', 'CTR_RightLungAffected', 'CTR_LungCapacityDecrease', 'CTR_Calcification',
+              'CTR_Pleurisy', 'CTR_Caverns']
+
+    for field in fields:
+        s = field[4:] + ' & '
+        s += str(sum(df1[field])) + ' \\\\'
+
+        print(s)
+
+
+svr_table()
+print('\n')
+ctr_table()
